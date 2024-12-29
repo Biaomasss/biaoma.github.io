b1aa8baf298570f5400277c654ed597d7af069fa
Biaomasss <121618120+Biaomasss@users.noreply.github.com>
2024-12-29 19:14:02 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 示例：将页面滚动到相应的部分
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
